432cdd442fef257fd218ddd63a027ef658fe92a4
Prasath  Ram <71248627+PrasathRamR@users.noreply.github.com>
2024-11-27 23:58:31 +0530

    Update Try.py

--- Try.py
+++ Try.py
@@ -3,6 +3,53 @@ import pandas as pd
 import numpy as np
 from streamlit_option_menu import option_menu
 import json
+import hmac
+import openpyxl
+
+st.set_page_config(
+    page_title="Auc-Biddy",  # Title of the app
+    page_icon="auc.png",  
+    layout="wide",  # Layout of the app ("centered" or "wide")
+    initial_sidebar_state="expanded"  # Sidebar state ("expanded" or "collapsed")
+)
+
+def check_password():
+    """Returns `True` if the user had a correct password."""
+
+    def login_form():
+        st.title("Auc-Buddy: Your Auction Companion")
+        with st.form("Credentials"):
+            st.text_input("Username", key="username")
+            st.text_input("Password", type="password", key="password")
+            st.form_submit_button("Log in", on_click=password_entered)
+        st.write("For registrations, please mailto: rpstram@gmail.com / praveenram.ramasubramani@gmail.com")
+
+    def password_entered():
+        """Checks whether a password entered by the user is correct."""
+        if st.session_state["username"] in st.secrets[
+            "passwords"
+        ] and hmac.compare_digest(
+            st.session_state["password"],
+            st.secrets.passwords[st.session_state["username"]],
+        ):
+            st.session_state["password_correct"] = True
+            del st.session_state["password"]  # Don't store the username or password.
+            del st.session_state["username"]
+        else:
+            st.session_state["password_correct"] = False
+
+    # Return True if the username + password is validated.
+    if st.session_state.get("password_correct", False):
+        return True
+
+    # Show inputs for username + password.
+    login_form()
+    if "password_correct" in st.session_state:
+        st.error("😕 User not known or password incorrect")
+    return False
+
+if not check_password():
+    st.stop()
 
 
 if "auction_list_file_df" not in st.session_state:
@@ -445,12 +492,7 @@ def retention():
 
 
 
-st.set_page_config(
-    page_title="Auc-Biddy",  # Title of the app
-    page_icon="auc.png",  
-    layout="wide",  # Layout of the app ("centered" or "wide")
-    initial_sidebar_state="expanded"  # Sidebar state ("expanded" or "collapsed")
-)
+
 
 
 selected = option_menu(
